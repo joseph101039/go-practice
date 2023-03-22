12d520325f3b602d58a8b44cad00f849276b6718
Joseph_Li <joseph_li@mail.chungyo.net>
2023-03-22 16:04:41 +0800

    git add  buffered channel test

---
+++ buffered_channel_test.go
@@ -0,0 +1,184 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"reflect"
+	"runtime"
+	"testing"
+	"time"
+)
+
+type Request struct {
+	args       []interface{}
+	f          func(...interface{}) interface{}
+	resultChan chan interface{}
+}
+
+
+func Test_bufferedChannel(t *testing.T) {
+	concurrency := runtime.GOMAXPROCS(0)
+	bufferedChannel(t, concurrency, 100)
+}
+
+
+func Benchmark_bufferedChannel(b *testing.B) {
+	bufferedChannel(b, 20, 100)
+}
+
+func parallel() {
+	// cpuNum := runtime.NumCPU()
+	
+
+
+}
+
+// semaphore 
+// for  rate-limited, parallel, (non-blocking ?) RPC system, and there's not a mutex in sight.
+func bufferedChannel(t testing.TB, concurrency int, testingLoop int) {
+
+	t.Logf("%d concurrency handlers and total %d requests\n", concurrency, testingLoop)
+	
+	resultChan := NewResultChannels(concurrency)	
+	reqChan := make(chan *Request, concurrency)
+
+	DispatchHandlers(reqChan, concurrency)
+	t.Logf("%d handlers are dispatched\n", concurrency)
+
+
+
+	var cases = make([]reflect.SelectCase, concurrency)
+
+	var process = func(parameters ...interface{}) (sum interface{}) {
+		r := 0
+		for _, e := range parameters {
+			r += e.(int)
+		}
+
+		time.Sleep(time.Second * time.Duration(rand.Intn(3) + 1))  // sleep random seconds
+		return r
+	}
+
+	// 模擬處理結果 (can be use as aggregate algorithm)
+	var resultCallback = func(index int, result reflect.Value) {
+		sum := result.Interface().(int)
+		fmt.Printf("result %d is output: %d\n", index, sum)
+
+	}
+
+
+	var result reflect.Value
+	for i := 0; i < testingLoop; i++ {	
+		
+		current := i % concurrency // default
+		
+
+		// get result if the request channel is full
+		if len(reqChan) == cap(reqChan) {
+			current, result, _ = FetchResult(&cases)
+			resultCallback(current, result) 
+		}
+
+
+
+		EnqueueRequest(&(cases[current]), reqChan, &Request{
+			args: []interface{} {
+					rand.Intn(10),
+					rand.Intn(10), 
+					rand.Intn(10),
+				},
+			f:          process,
+			resultChan: resultChan[current],
+		})
+
+
+		t.Logf("the %d th request are send", i + 1)
+	}
+
+	// todo some results may not be fetched
+
+	// 取出剩餘結果
+	for len(reqChan) != 0 {
+		chosen, result, _ := FetchResult(&cases)
+		resultCallback(chosen, result) 
+	}
+
+
+	
+	close(reqChan)  // to make all recievers finishing waiting
+	t.Logf("There are %d goroutines are currently running", runtime.NumGoroutine())
+}
+
+
+func EnqueueRequest(rcase *reflect.SelectCase, reqChan chan <- *Request, req *Request) {
+	// push a new request case into queue
+	*rcase = reflect.SelectCase{
+		Dir:reflect.SelectRecv,
+		Chan: reflect.ValueOf(req.resultChan),
+	}
+
+	reqChan <- req
+}
+
+func FetchResult(cases *[]reflect.SelectCase) (chosen int, result reflect.Value, err error) {
+	chosen, result, ok  := reflect.Select(*cases)
+			
+	if !ok {
+		// The chosen channel has been closed, so zero out the channel to disable the case
+		(*cases)[chosen].Chan = reflect.ValueOf(nil)
+		return -1, reflect.Value{}, fmt.Errorf("[Error] cannot receive the result of case %d", chosen)
+	}
+
+	// 模擬處理結果 (can be use as aggregate algorithm)
+	return chosen, result, nil
+}
+
+
+
+
+func NewResultChannels(concurrency int) []chan interface{} {
+	resultChan := make([]chan interface{}, concurrency)
+	for i := range resultChan {
+		resultChan[i] = make(chan interface{})
+	}
+	return resultChan
+}
+
+
+func DispatchHandlers(reqChan <-chan *Request, concurrency int) {
+
+	for i := 0; i < concurrency; i++ {
+		go handleWithTimeout(reqChan)	
+		// go handle(reqChan)
+	}
+	
+}
+
+
+// normal handle
+func handle(reqChan <-chan *Request) {
+	var req *Request = nil
+	for req = range reqChan {  // break if the reqChan is closed
+		req.resultChan <- req.f(req.args ...)
+	}
+}
+
+
+// normal handle plus timeout panic
+func handleWithTimeout(reqChan <-chan *Request) {
+	timeout := time.After(30 * time.Second)
+	for {
+		select {
+			case req, ok := <- reqChan :  // 這邊需要 ok 確認 channel 是否已經關閉
+				if !ok {
+					reqChan = nil // prevent the channel from sending more data
+					break
+				}
+				req.resultChan <- req.f(req.args ...)
+			
+			case <-timeout:  // if timeout
+				err := fmt.Errorf("Reciever timeouts for waiting any incoming request")
+				panic(err)  // or break the loop if you want
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/bytedance/sonic v1.8.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/go-playground/validator/v10 v10.11.2 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/securecookie v1.1.1 // indirect
@@ -36,15 +38,26 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/smartystreets/assertions v1.2.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.9 // indirect
 	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
 	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/exp v0.0.0-20221031165847-c99f073a8326 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221031165847-c99f073a8326 // indirect
+	golang.org/x/mod v0.7.0 // indirect
 	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/tools v0.3.1-0.20221213193459-ca17b2c27ca8 // indirect
+	golang.org/x/tools/gopls v0.11.0 // indirect
+	golang.org/x/vuln v0.0.0-20221109205719-3af8368ee4fe // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/tools v0.3.3 // indirect
+	mvdan.cc/gofumpt v0.4.0 // indirect
+	mvdan.cc/xurls/v2 v2.4.0 // indirect
 )
